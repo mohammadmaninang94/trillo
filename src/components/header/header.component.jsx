0f65c8004c29c417b593557c69c4640a4349fea1
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-12-30 14:48:01 +0800

    Created ne trillo project focusing on flexbox, added header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,35 @@
+import './header.styles.scss';
+
+const Header = () => (
+    <header className='header'>
+        <img src="img/logo.png" alt="trillo logo" className="logo" />
+        <form action="#" className="search">
+            <input type="text" className="search__input" placeholder="Search hotels" />
+            <button className="search__button">
+                <svg className="search__icon">
+                    <use xlinkHref="img/sprite.svg#icon-magnifying-glass"></use>
+                </svg>
+            </button>
+        </form>
+        <nav className="user-nav">
+            <div className="user-nav__icon-box">
+                <svg className="user-nav__icon">
+                    <use xlinkHref="img/sprite.svg#icon-bookmark"></use>
+                </svg>
+                <span className="user-nav__notification">7</span>
+            </div>
+            <div className="user-nav__icon-box">
+                <svg className="user-nav__icon">
+                    <use xlinkHref="img/sprite.svg#icon-chat"></use>
+                </svg>
+                <span className="user-nav__notification">13</span>
+            </div>
+            <div className="user-nav__user">
+                <img src="img/user.jpg" alt="User" className="user-nav__user-photo" />
+                <span className="user-nav__user-name">Jonas</span>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Header;
---
+++ src/components/sidebar/sidebar.component.jsx
@@ -0,0 +1,9 @@
+import './sidebar.styles.scss';
+
+const Sidebar = () => (
+    <nav className='sidebar'>
+        Navigation
+    </nav>
+);
+
+export default Sidebar;

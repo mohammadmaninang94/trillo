1c8b71ae0e7b60ecfa111148110132750f108f37
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-01-08 11:22:57 +0800

    added cta section to finish off home page

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ function App() {
               <button className='btn-inline'>Show all <span>&rarr;</span></button>
             </div>
           </div>
+          <div className='cta'>
+            <h2 className='cta__book-now'>Good news! We have 4 free rooms for your selected dates!</h2>
+            <button type='button' className='btn'>
+                <span className='btn__visible'>Book Now</span>
+                <span className='btn__invisible'>Only 4 rooms left</span>
+            </button>
+          </div>
         </main>
       </div>
     </div>
